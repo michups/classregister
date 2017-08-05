2c45cbf4d9124dc7f9114031845aeb6ef969e9ef
michal <michalwir@gmail.com>
2017-08-05 22:29:29 +0200

    Add formatter support for Grade

---
+++ src/main/java/com/michups/classregister/formatter/GradeFormatter.java
@@ -0,0 +1,30 @@
+package com.michups.classregister.formatter;
+
+import com.michups.classregister.entity.Grade;
+import com.michups.classregister.service.GradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+/**
+ * Created by michups on 05.08.17.
+ */
+@Component
+public class GradeFormatter implements Formatter<Grade> {
+
+    @Autowired
+    private GradeService gradeService;
+
+    @Override
+    public String print(Grade grade, Locale locale) {
+        return grade.getNumber()+grade.getAlfabetDigit();
+    }
+
+    @Override
+    public Grade parse(String s, Locale locale) throws ParseException {
+        return gradeService.getGrade(Integer.valueOf(s));
+    }
+}
